725d91aac76fb759c59cdf83e301f3d15692389f
shibbo <supershibbo12@gmail.com>
2025-03-14 20:24:16 -0400

    start implementing some `SensorMsg` instances

--- lib/al/src/Library/Actor/ActorSensorUtil.cpp
+++ lib/al/src/Library/Actor/ActorSensorUtil.cpp
@@ -8,110 +8,117 @@
 
 namespace al {
 
-s32 getHitSensorNum(const LiveActor* pActor) {
-    return pActor->mHitSensorKeeper->mSensorCount;
-}
+    SENSOR_MSG_CHECK(EnemyAttackKnockDown);
+    SENSOR_MSG_CHECK(EnemyAttackBoomerang);
+    SENSOR_MSG_CHECK(EnemyRouteDokanAttack);
+    SENSOR_MSG_CHECK(EnemyRouteDokanFire);
+    SENSOR_MSG_CHECK(Explosion);
+    SENSOR_MSG_CHECK(ExplosionCollide);
 
-// void setHitSensorSort(LiveActor *pActor, const char *pSensorName, const SensorSortCmpFuncBase *)
+    s32 getHitSensorNum(const LiveActor* pActor) {
+        return pActor->mHitSensorKeeper->mSensorCount;
+    }
 
-void setHitSensorPosPtr(LiveActor* pActor, const char* pSensorName, const sead::Vector3f* pPosPtr) {
-    pActor->mHitSensorKeeper->getSensor(pSensorName)->setFollowPosPtr(pPosPtr);
-}
+    // void setHitSensorSort(LiveActor *pActor, const char *pSensorName, const SensorSortCmpFuncBase *)
 
-HitSensor* getHitSensor(const LiveActor* pActor, const char* pSensorName) {
-    return pActor->mHitSensorKeeper->getSensor(pSensorName);
-}
+    void setHitSensorPosPtr(LiveActor* pActor, const char* pSensorName, const sead::Vector3f* pPosPtr) {
+        pActor->mHitSensorKeeper->getSensor(pSensorName)->setFollowPosPtr(pPosPtr);
+    }
 
-void setHitSensorMtxPtr(LiveActor* pActor, const char* pSensorName, const sead::Matrix34f* pMtx) {
-    pActor->mHitSensorKeeper->getSensor(pSensorName)->setFollowMtxPtr(pMtx);
-}
+    HitSensor* getHitSensor(const LiveActor* pActor, const char* pSensorName) {
+        return pActor->mHitSensorKeeper->getSensor(pSensorName);
+    }
 
-void setHitSensorJointMtx(LiveActor* pActor, const char* pSensorName, const char* pJointName) {
-    pActor->mHitSensorKeeper->getSensor(pSensorName)
-        ->setFollowMtxPtr(getJointMtxPtr(pActor, pJointName));
-}
+    void setHitSensorMtxPtr(LiveActor* pActor, const char* pSensorName, const sead::Matrix34f* pMtx) {
+        pActor->mHitSensorKeeper->getSensor(pSensorName)->setFollowMtxPtr(pMtx);
+    }
 
-const sead::Matrix34f* getHitSensorFollowMtx(HitSensor* pSensor) {
-    return pSensor->mFollowPosMtx;
-}
+    void setHitSensorJointMtx(LiveActor* pActor, const char* pSensorName, const char* pJointName) {
+        pActor->mHitSensorKeeper->getSensor(pSensorName)
+            ->setFollowMtxPtr(getJointMtxPtr(pActor, pJointName));
+    }
 
-HitSensor* getHitSensor(const LiveActor* pActor, s32 sensorIdx) {
-    return pActor->mHitSensorKeeper->getSensor(sensorIdx);
-}
+    const sead::Matrix34f* getHitSensorFollowMtx(HitSensor* pSensor) {
+        return pSensor->mFollowPosMtx;
+    }
 
-void setSensorRadius(LiveActor* pActor, const char* pSensorName, f32 radius) {
-    HitSensor* sensor = pActor->mHitSensorKeeper->getSensor(pSensorName);
-    sensor->mRadius = radius;
-}
+    HitSensor* getHitSensor(const LiveActor* pActor, s32 sensorIdx) {
+        return pActor->mHitSensorKeeper->getSensor(sensorIdx);
+    }
 
-void setSensorRadius(LiveActor* pActor, s32 sensorIdx, f32 radius) {
-    HitSensor* sensor = pActor->mHitSensorKeeper->getSensor(sensorIdx);
-    sensor->mRadius = radius;
-}
+    void setSensorRadius(LiveActor* pActor, const char* pSensorName, f32 radius) {
+        HitSensor* sensor = pActor->mHitSensorKeeper->getSensor(pSensorName);
+        sensor->mRadius = radius;
+    }
 
-void setSensorRadius(LiveActor* pActor, f32 radius) {
-    HitSensor* sensor = pActor->mHitSensorKeeper->getSensor(nullptr);
-    sensor->mRadius = radius;
-}
+    void setSensorRadius(LiveActor* pActor, s32 sensorIdx, f32 radius) {
+        HitSensor* sensor = pActor->mHitSensorKeeper->getSensor(sensorIdx);
+        sensor->mRadius = radius;
+    }
 
-f32 getSensorRadius(const LiveActor* pActor, const char* pSensorName) {
-    return pActor->mHitSensorKeeper->getSensor(pSensorName)->mRadius;
-}
+    void setSensorRadius(LiveActor* pActor, f32 radius) {
+        HitSensor* sensor = pActor->mHitSensorKeeper->getSensor(nullptr);
+        sensor->mRadius = radius;
+    }
 
-f32 getSensorRadius(const LiveActor* pActor, s32 sensorIdx) {
-    return pActor->mHitSensorKeeper->getSensor(sensorIdx)->mRadius;
-}
+    f32 getSensorRadius(const LiveActor* pActor, const char* pSensorName) {
+        return pActor->mHitSensorKeeper->getSensor(pSensorName)->mRadius;
+    }
 
-f32 getSensorRadius(const LiveActor* pActor) {
-    return pActor->mHitSensorKeeper->getSensor(nullptr)->mRadius;
-}
+    f32 getSensorRadius(const LiveActor* pActor, s32 sensorIdx) {
+        return pActor->mHitSensorKeeper->getSensor(sensorIdx)->mRadius;
+    }
 
-void setSensorFollowPosOffset(LiveActor* pActor, const char* pSensorName,
-                              const sead::Vector3f& rOffset) {
-    getHitSensor(pActor, pSensorName)->mFollowPosOffset.set(rOffset);
-}
+    f32 getSensorRadius(const LiveActor* pActor) {
+        return pActor->mHitSensorKeeper->getSensor(nullptr)->mRadius;
+    }
 
-void setSensorFollowPosOffset(LiveActor* pActor, s32 sensorIdx, const sead::Vector3f& rOffset) {
-    getHitSensor(pActor, sensorIdx)->mFollowPosOffset.set(rOffset);
-}
+    void setSensorFollowPosOffset(LiveActor* pActor, const char* pSensorName,
+                                const sead::Vector3f& rOffset) {
+        getHitSensor(pActor, pSensorName)->mFollowPosOffset.set(rOffset);
+    }
 
-void setSensorFollowPosOffset(LiveActor* pActor, const sead::Vector3f& rOffset) {
-    getHitSensor(pActor, nullptr)->mFollowPosOffset.set(rOffset);
-}
+    void setSensorFollowPosOffset(LiveActor* pActor, s32 sensorIdx, const sead::Vector3f& rOffset) {
+        getHitSensor(pActor, sensorIdx)->mFollowPosOffset.set(rOffset);
+    }
 
-sead::Vector3f* getSensorFollowPosOffset(const LiveActor* pActor, const char* pSensorName) {
-    return &pActor->mHitSensorKeeper->getSensor(pSensorName)->mFollowPosOffset;
-}
+    void setSensorFollowPosOffset(LiveActor* pActor, const sead::Vector3f& rOffset) {
+        getHitSensor(pActor, nullptr)->mFollowPosOffset.set(rOffset);
+    }
 
-sead::Vector3f* getSensorFollowPosOffset(const LiveActor* pActor, s32 sensorIdx) {
-    return &pActor->mHitSensorKeeper->getSensor(sensorIdx)->mFollowPosOffset;
-}
+    sead::Vector3f* getSensorFollowPosOffset(const LiveActor* pActor, const char* pSensorName) {
+        return &pActor->mHitSensorKeeper->getSensor(pSensorName)->mFollowPosOffset;
+    }
 
-sead::Vector3f* getSensorFollowPosOffset(const LiveActor* pActor) {
-    return &pActor->mHitSensorKeeper->getSensor(nullptr)->mFollowPosOffset;
-}
+    sead::Vector3f* getSensorFollowPosOffset(const LiveActor* pActor, s32 sensorIdx) {
+        return &pActor->mHitSensorKeeper->getSensor(sensorIdx)->mFollowPosOffset;
+    }
 
-ActorSensorController* createActorSensorController(LiveActor* pActor, const char* pName) {
-    return new ActorSensorController(pActor, pName);
-}
+    sead::Vector3f* getSensorFollowPosOffset(const LiveActor* pActor) {
+        return &pActor->mHitSensorKeeper->getSensor(nullptr)->mFollowPosOffset;
+    }
 
-void setSensorRadius(ActorSensorController* pController, f32 radius) {
-    pController->setSensorRadius(radius);
-}
+    ActorSensorController* createActorSensorController(LiveActor* pActor, const char* pName) {
+        return new ActorSensorController(pActor, pName);
+    }
 
-void setSensorFollowPosOffset(ActorSensorController* pController, const sead::Vector3f& rOffs) {
-    pController->setSensorFollowPosOffset(rOffs);
-}
+    void setSensorRadius(ActorSensorController* pController, f32 radius) {
+        pController->setSensorRadius(radius);
+    }
 
-f32 getOriginalSensorRadius(const ActorSensorController* pController) {
-    return pController->mSensorRadius;
-}
+    void setSensorFollowPosOffset(ActorSensorController* pController, const sead::Vector3f& rOffs) {
+        pController->setSensorFollowPosOffset(rOffs);
+    }
 
-const sead::Vector3f* getOriginalSensorFollowPosOffset(const ActorSensorController* pController) {
-    return &pController->mFollowPosOffs;
-}
+    f32 getOriginalSensorRadius(const ActorSensorController* pController) {
+        return pController->mSensorRadius;
+    }
 
-void resetActorSensorController(ActorSensorController* pController) {
-    pController->resetActorSensorController();
-}
+    const sead::Vector3f* getOriginalSensorFollowPosOffset(const ActorSensorController* pController) {
+        return &pController->mFollowPosOffs;
+    }
+
+    void resetActorSensorController(ActorSensorController* pController) {
+        pController->resetActorSensorController();
+    }
 }  // namespace al
